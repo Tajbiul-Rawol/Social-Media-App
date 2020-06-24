7dc02cd8aade560c0079b88919e2bcc3490a21db
Rawol Tajbiul <tajbiulrawol@gmail.com>
2020-06-25 01:08:41 +0600

    login successful

--- index.php
+++ index.php
@@ -12,54 +12,45 @@
 
  <?php 
 
-      if (isset($_POST['login'])) {
+      if (isset($_POST['sigin'])) {
           
-           $ue =  $_POST['ue'];
-           $pass = $_POST['pass'];
+          //value get
+            $ue =  $_POST['ue'];
+            $pass = $_POST['pass'];
 
-           if (empty($ue) || empty($pass)) {
-               
-               $mess = '<p class="alert alert-danger">All fields are required to login!!<button data-dismiss="alert" class="close"> &times; </button></p>';    
+            if (empty($ue) || empty($pass)) {
+              
+                 $mess = '<p class="alert alert-danger"> All fields required! <button data-dismiss="alert" class="close"> &times; </button></p>';
+            }else{
 
-           }else{
+                 $sql = "SELECT * FROM users WHERE username='$ue' OR email='$ue' ";
+                 $data = $connection -> query($sql);
+                 $login_user_data = $data -> fetch_assoc();
+                 $count = $data -> num_rows;
 
-               $sql = "SELECT * FROM users WHERE username = '$ue' OR email = '$ue' ";
-               $data = $connection -> query($sql);
-               $user_data = $data -> fetch_assoc();
-               
-               //print_r($data); //prints out the mysqli object
-               //print_r($user_data); //prints out the fetched data from the object
-               
-               $count = $data -> num_rows;
 
-               if ($count == 1) {
-                     
-                     $hash = $user_data['password'];
+                  //print_r($login_user_data );
+                   
+                 if ($count == 1 ) {
                       
-                    //check if the password hash match with the password
-                     if ( password_verify($pass, $hash) == true) {
-                          
-                          echo "condition e dhuktese"; 
-                          
-                          header('location:Profile.php');
-                      }else{
-                          
-                          echo "condition e dhuktese na";
-
-                          //print_r( $user_data['password']);
-
-                          $mess = '<p class="alert alert-danger"> Password incorrect!<button data-dismiss="alert" class="close"> &times; </button></p>';  
-                      } 
-
-               }else{
                      
-                     $mess = '<p class="alert alert-danger"> Username or email incorrect!!<button data-dismiss="alert" class="close"> &times; </button></p>';  
+                     $pass_verify = password_verify($pass , $login_user_data['password'] ); 
                       
-               }
+                     if ($pass_verify == true) {
+                       
+                         header('location:Profile.php');
+                     }else{
 
+                         $mess = '<p class="alert alert-danger"> Wrong password! <button data-dismiss="alert" class="close"> &times; </button></p>';
+                     }
 
+                 }else{
 
-           }
+                     $mess = '<p class="alert alert-danger"> Wrong username or email! <button data-dismiss="alert" class="close"> &times; </button></p>';
+                 }
+            }
+
+           
 
       }
 
@@ -92,7 +83,7 @@
                    ?>
 
 
-                 <form action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="POST"  enctype="multipart/form-data">
+                 <form action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="POST" >
                  	 
 
                       <div class="form-group">
@@ -107,12 +98,11 @@
                       	
                       </div>
 
-             
 
 
                       <div class="form-group">
                       	
-                      	<input class="btn btn-info" type="submit" name="login" value="Log in" >
+                      	<input class="btn btn-info" type="submit" name="sigin" value="Sign in" >
                       	
                       </div>
 
